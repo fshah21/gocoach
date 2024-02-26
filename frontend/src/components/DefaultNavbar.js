73f6f7e8f93d422755f640607cf3414c6afc6b54
Feya Shah <feya.shah@askpinkypromise.com>
2024-02-26 22:59:26 +0530

    Navbar logout / login

--- frontend/src/components/DefaultNavbar.js
+++ frontend/src/components/DefaultNavbar.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
 
 const DefaultNavbar = () => {
+  const userId = useSelector(state => state.user.userId);
+
   return (
     <Navbar bg="dark" variant="dark">
       <Navbar.Brand className="justify-content-start ms-4" href="#home">GoCoach</Navbar.Brand>
       <Nav className="justify-content-end me-4" style={{ width: "100%" }}>
-        <Nav.Link href="/signup">Sign Up</Nav.Link>
-        <Nav.Link href="/login">Login</Nav.Link>
+        {userId ? (
+            // If userId is present, show Logout button
+            <Nav.Link href="/logout">Logout</Nav.Link>
+          ) : (
+            // If userId is not present, show Sign Up and Login buttons
+            <>
+              <Nav.Link href="/signup">Sign Up</Nav.Link>
+              <Nav.Link href="/login">Login</Nav.Link>
+            </>
+          )}
       </Nav>
     </Navbar>
   );
