6717f5fb34ccb4ca1886bdc17c80d5551176d05e
Feya Shah <feya.shah@askpinkypromise.com>
2024-03-02 16:22:14 +0530

    Adding dashboard code

--- frontend/src/components/UserHome.js
+++ frontend/src/components/UserHome.js
@@ -10,6 +10,9 @@ const UserHome = () => {
   console.log("USER ID IN USER HOME", userId);
   const [classInfo, setClassInfo] = useState(null);
   const [sections, setSections] = useState(null);
+  const [pastClassIds, setPastClassIds] = useState([]);
+  const [pastSections, setPastSections] = useState([]);
+  const [pastClasses, setPastClasses] = useState([]);
 
   useEffect(() => {
     const fetchClassAndSections = async () => {
@@ -25,13 +28,31 @@ const UserHome = () => {
         const sectionsResponse = await axios.get('http://localhost:5000/gocoachbackend/us-central1/backend/users/'+ userId + '/classes/getAllSectionsInClass/' + classId);
         console.log("SECTION RESPONSE DATA", sectionsResponse.data);
         setSections(sectionsResponse.data);
+
+        const pastClassesResponse = await axios.get(`http://localhost:5000/gocoachbackend/us-central1/backend/classes/${userId}/getPastClasses`);
+        console.log("PAST CLASSES RESPONSE DATA", pastClassesResponse.data);
+        setPastClasses(pastClassesResponse.data.slice(0, 2));
+        const classIds = pastClassesResponse.data.map(pastClass => pastClass.id);
+        setPastClassIds(classIds.slice(0, 2));
+        console.log("PAST CLASS IDS", classIds);
+
+        const pastSectionsPromises = pastClassIds.map(async classId => {
+          console.log("PAST CLASS ID", classId);
+          const sectionsResponse = await axios.get(`http://localhost:5000/gocoachbackend/us-central1/backend/users/${userId}/classes/getAllSectionsInClass/${classId}`);
+          return sectionsResponse.data;
+        });
+
+        const resolvedPastSections = await Promise.all(pastSectionsPromises);
+        console.log("PAST SECTIONS RESPONSE DATA", resolvedPastSections);
+        setPastSections(resolvedPastSections);
+
       } catch (error) {
         console.error('Error fetching data:', error);
       }
     };
 
     fetchClassAndSections();
-  }, []);
+  }, [userId, pastClassIds]);
 
   return (
     <>
@@ -81,20 +102,64 @@ const UserHome = () => {
                       <Row>
                         {/* First Recent Card */}
                         <Row className="mb-3">
-                          <div style={{ height: '50%' }} className="mt-5">
+                          <Card className="mt-5">
                             {/* Your content for the first Recent Card goes here */}
-                            <h2>Recent</h2>
+                            <Card.Header>
+                              <h2>Recent</h2>
+                            </Card.Header>
+                            <Card.Body>
+                            {pastClasses[0] && (
+                              <>
+                                <Card.Title>{pastClasses[0].className}</Card.Title>
+                                <Card.Text>{pastClasses[0].classDescription}</Card.Text>
+                              </>
+                            )}
+
+                            {pastSections[0] && (
+                              <ListGroup variant="flush">
+                                {pastSections[0].map(section => (
+                                  <ListGroup.Item key={section.sectionId}>
+                                    <h5 className='fw-bold'>{section.name} | {`${section.startTime} - ${section.finishTime}`} Mins </h5> 
+                                    <p>{section.displayText}</p>
+                                    <p>{section.coachNotes}</p>
+                                  </ListGroup.Item>
+                                ))}
+                              </ListGroup>
+                            )}
+                          </Card.Body>
                             {/* First small card content */}
-                          </div>
+                          </Card>
                         </Row>
 
                         {/* Second Recent Card */}
                         <Row>
-                          <div style={{ height: '50%' }}  className="mt-5">
-                            {/* Your content for the second Recent Card goes here */}
-                            {/* Second small card content */}
-                            <h2>Recent 2</h2>
-                          </div>
+                        <Card className="mt-5">
+                            {/* Your content for the first Recent Card goes here */}
+                            <Card.Header>
+                              <h2>Recent</h2>
+                            </Card.Header>
+                            <Card.Body>
+                            {pastClasses[0] && (
+                              <>
+                                <Card.Title>{pastClasses[1].className}</Card.Title>
+                                <Card.Text>{pastClasses[1].classDescription}</Card.Text>
+                              </>
+                            )}
+
+                            {pastSections[1] && (
+                              <ListGroup variant="flush">
+                                {pastSections[1].map(section => (
+                                  <ListGroup.Item key={section.sectionId}>
+                                    <h5 className='fw-bold'>{section.name} | {`${section.startTime} - ${section.finishTime}`} Mins </h5> 
+                                    <p>{section.displayText}</p>
+                                    <p>{section.coachNotes}</p>
+                                  </ListGroup.Item>
+                                ))}
+                              </ListGroup>
+                            )}
+                          </Card.Body>
+                            {/* First small card content */}
+                          </Card>
                         </Row>
                       </Row>
                     </Col>
